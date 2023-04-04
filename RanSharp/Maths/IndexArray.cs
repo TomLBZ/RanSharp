97a12c2461278d61d55b530a06dc9509367b116a
TomLBZ <libozhao142857@hotmail.com>
2023-04-04 14:57:14 +0800

    Updated package to include the IndexArray<T> type.

---
+++ RanSharp/Maths/IndexArray.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace RanSharp.Maths
+{
+    /// <summary>
+    /// A wrapper on the Array type which overloads the Equals() method and returns true only if all elements are equal.
+    /// This struct is intended to be used with the IndexVar&lt;T&gt; type to prevent repeating elements in the indexed variable.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public readonly struct IndexArray<T> where T : struct, INumber<T>
+    {
+        private readonly T[] data;
+        /// <summary>
+        /// A constructor for the IndexVar&lt;T&gt; struct based on the provided array of data.
+        /// </summary>
+        /// <param name="data"></param>
+        public IndexArray(T[] data)
+        {
+            this.data = data;
+        }
+        /// <summary>
+        /// A read-only pseudo indexer. It used to access the elements of the inner array.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
+        /// <summary>
+        /// Returns true if all elements of both objects are equal in value.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj == null) return false;
+            if (obj is not IndexArray<T>) return false;
+            IndexArray<T> other = (IndexArray<T>)obj;
+            if (data.Length != other.data.Length) return false;
+            bool result = true;
+            for (int i  = 0; i < data.Length; i++)
+                result &= data[i] == other.data[i];
+            return result;
+        }
+        /// <summary>
+        /// Gets the hashcode of the inner data array.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return data.GetHashCode();
+        }
+        /// <summary>
+        /// Compars the equality of both operands using the overloaded Equals operator.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public static bool operator ==(IndexArray<T> lhs, IndexArray<T> rhs) => lhs.Equals(rhs);
+        /// <summary>
+        /// Compars the inequality of both operands using the overloaded Equals operator.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public static bool operator !=(IndexArray<T> lhs, IndexArray<T> rhs) => !lhs.Equals(rhs);
+        /// <summary>
+        /// Explicits converts an IndexVar&lt;T&gt; to an array of <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="original"></param>
+        public static explicit operator T[](IndexArray<T> original) => original.data;
+        /// <summary>
+        /// Explicits converts an array of <typeparamref name="T"/> to an IndexVar&lt;T&gt;
+        /// </summary>
+        /// <param name="values"></param>
+        public static explicit operator IndexArray<T>(T[] values) => new(values);
+    }
+}
--- RanSharp/Maths/IndexVar.cs
+++ RanSharp/Maths/IndexVar.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Numerics;
+using RanSharp.Performance;
 
 namespace RanSharp.Maths
 {
@@ -6,21 +8,22 @@ namespace RanSharp.Maths
     /// A variable with indices. Can be used in symbolic calculations, or as a dictionary key.<br/>
     /// For example:
     /// <code>
-    /// IndexVar&lt; int&gt; x = new(2); // Represents an indexed variable based on 2 <see cref="int"/> values.<br/>
-    /// List&lt;int[]&gt; symbolicPoints = new() { x[0, 0], x[0, 1]}; // Represents a list of 2D points: x_00 and x_01<br/>
-    /// Func&lt;int[], PointF&gt; valueMap = (indices) => {/*body*/}; // Represents a function that maps an indexed variable to its value.<br/>
+    /// IndexVar&lt;int&gt; x = new(2); // Represents an indexed variable based on 2 <see cref="int"/> values.<br/>
+    /// List&lt;IndexArray&lt;int&gt;&gt; symbolicPoints = new() { x[0, 0], x[0, 1]}; // Represents a list of 2D points: x_00 and x_01<br/>
+    /// Func&lt;IndexArray&lt;int&gt;, PointF&gt; valueMap = (indices) => {/*body*/}; // Represents a function that maps an indexed variable to its value.<br/>
     /// List&lt;PointF&gt; points = points.Select(valueMap).ToList(); // Evaluates the actual values of the points.<br/>
     /// </code>
     /// When the value map is a simple function, the <see cref="IndexVar{T}"/> can be used as a dictionary key:
     /// <code>
-    /// Dictionary&lt;IndexVar&lt;int&gt;, PointF&gt; points = new() { { x[1, 2], new PointF(1, 2) }, { x[3, 4], new PointF(3, 4) }, { x[5, 6], new PointF(5, 6) } }; // Represents a list of 2D points.<br/>
+    /// Dictionary&lt;IndexArray&lt;int&gt;, PointF&gt; points = new() { { x[1, 2], new PointF(1, 2) }, { x[3, 4], new PointF(3, 4) }, { x[5, 6], new PointF(5, 6) } }; // Represents a list of 2D points.<br/>
     /// </code>
     /// When the value map is very complicated and performance is critical, the <see cref="IndexVar{T}"/> should be used instead of variable values, 
     /// because it avoids the expensive evaluation as much as possible. The value map only needs to be used once after all symbolic calculations has been done.
     /// </summary>
-    public readonly struct IndexVar<T> : IEnumerable<T[]>
+    /// <typeparam name="T"></typeparam>
+    public readonly struct IndexVar<T> : IEnumerable<IndexArray<T>> where T : struct, INumber<T>
     {
-        private readonly HashSet<T[]> _values = new();
+        private readonly HashSet<IndexArray<T>> _values = new();
         /// <summary>
         /// Gets the number of unique indices that has been stored.
         /// </summary>
@@ -28,7 +31,7 @@ namespace RanSharp.Maths
         /// <summary>
         /// Gets the list of unique indices that has been stored.
         /// </summary>
-        public List<T[]> Values => _values.ToList();
+        public FastList<IndexArray<T>> Values => new(_values);
         /// <summary>
         /// Gets the length of each index.
         /// </summary>
@@ -36,6 +39,7 @@ namespace RanSharp.Maths
         /// <summary>
         /// Initializes a new instance of the <see cref="IndexVar{T}"/> struct using the specified index length.
         /// </summary>
+        /// <param name="indexLen"></param>
         public IndexVar(int indexLen) { IndexLength = indexLen; }
         /// <summary>
         /// The read-only pseudo indexer. It is used as a readable short hand for referencing indexed variables. Do not use it as an actual indexer.
@@ -44,7 +48,8 @@ namespace RanSharp.Maths
         /// Incorrect usage:
         /// <code>x[0] = 5; // Wrong attempt to assign a value to the variable x0</code>
         /// </summary>
-        public T[] this[params T[] index]
+        /// <param name="index"></param>
+        public IndexArray<T> this[params T[] index]
         {
             get
             {
@@ -52,25 +57,27 @@ namespace RanSharp.Maths
                     throw new ArgumentNullException(nameof(index));
                 if (index.Length != IndexLength)
                     throw new ArgumentException($"Index length must be {IndexLength}.");
-                _ = _values.Add(index);
-                return index;
+                IndexArray<T> id = (IndexArray<T>)index;
+                _ = _values.Add(id);
+                return id;
             }
         }
         /// <summary>
         /// Removes the specified index from the list of unique indices.
         /// </summary>
+        /// <param name="index"></param>
         public void Remove(params T[] index)
         {
             if (null == index)
                 throw new ArgumentNullException(nameof(index));
             if (index.Length != IndexLength)
                 throw new ArgumentException($"Index length must be {IndexLength}.");
-            _values.Remove(index);
+            _values.Remove((IndexArray<T>)index);
         }
         /// <summary>
         /// Returns an enumerator that iterates through the collection.
         /// </summary>
         public IEnumerator GetEnumerator() => _values.GetEnumerator();
-        IEnumerator<T[]> IEnumerable<T[]>.GetEnumerator() => _values.GetEnumerator();
+        IEnumerator<IndexArray<T>> IEnumerable<IndexArray<T>>.GetEnumerator() => _values.GetEnumerator();
     }
 }
